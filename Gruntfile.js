17e89a785c69ca3e7fd4059465050c78d1e949f8
Charlie Owen <charlie@sonniesedge.co.uk>
2014-10-23 13:00:34 +0100

    Still trying to figure this out

--- Gruntfile.js
+++ Gruntfile.js
@@ -32,8 +32,12 @@ module.exports = function (grunt) {
     },
     // Configuration to be run (and then tested).
     inlinestyles: {
+      options: {
+        
+      },
       absolutepath_basic: {
         options: {
+          basepath: 'changed'
         },
         files: {
           'tmp/absolutepath_basic': 'test/fixtures/page_absolute_path_nohost.html'
@@ -56,30 +60,46 @@ module.exports = function (grunt) {
         }
       },
 
-      relativepath_globbing: {
-        options: {
-        },
-        files: {
-          'tmp/relativepath_globbing': 'test/fixtures/page_relative_path_nohost.html'
-        }
-      },
+      // relativepath_mixed: {
+      //   options: {
+      //   },
+      //   files: [
+      //   {src: ['lib/a.js'], dest: 'build/a.min.js'},
+      //   {src: 'lib/b.js', dest: 'build/b.min.js'},
+      //   {src: 'lib/subdir/c.js', dest: 'build/subdir/c.min.js'},
+      //   {src: 'lib/subdir/d.js', dest: 'build/subdir/d.min.js'},
+      // ],
+      // },
 
-
+      // relativepath_globbing: {
+      //   options: {
+      //   },
+      //   files: [
+      //   {
+      //     expand: true,     // Enable dynamic expansion.
+      //     cwd: 'test/fixtures/globbing/',      // Src matches are relative to this path.
+      //     src: ['**/*.html'], // Actual pattern(s) to match.
+      //     dest: 'tmp/',   // Destination path prefix.
+      //   },
+      // ],
+      // },
+      //
+      //
       absoluteuri_basic: {
         options: {
         },
         files: {
-          'tmp/absoluteuri_basic': 'test/fixtures/page_absolute_uri_nohost.html'
+          'tmp/absoluteuri_basic': 'test/fixtures/page_absolute_uri.html'
         }
       },
-
-      absoluteuri_globbing: {
-        options: {
-        },
-        files: {
-          'tmp/absoluteuri_globbing': 'test/fixtures/page_absolute_uri_nohost.html'
-        }
-      }
+      //
+      // absoluteuri_globbing: {
+      //   options: {
+      //   },
+      //   files: {
+      //     'tmp/absoluteuri_globbing': 'test/fixtures/page_absolute_uri_nohost.html'
+      //   }
+      // }
 
 
     },
--- tasks/inlinestyles.js
+++ tasks/inlinestyles.js
@@ -1,8 +1,8 @@
 /*
  * grunt-inlinestyles
- * 
  *
- * Copyright (c) 2014 
+ *
+ * Copyright (c) 2014
  * Licensed under the MIT license.
  */
 
@@ -13,39 +13,102 @@ module.exports = function (grunt) {
   // Please see the Grunt documentation for more information regarding task
   // creation: http://gruntjs.com/creating-tasks
 
+
+  // Load helper modules
+  var path = require('path');
+  var cheerio = require('cheerio');
+  var url = require('url');
+
+
   grunt.registerMultiTask('inlinestyles', 'The best Grunt plugin ever.', function () {
 
-    // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
-      punctuation: '.',
-      separator: ', '
+      jsDir: "",
+      cssDir: "",
+      minify: false
     });
+
+grunt.log.writeln(JSON.stringify(options));
 
     // Iterate over all specified file groups.
     this.files.forEach(function (file) {
-      // Concat specified files.
-      var src = file.src.filter(function (filepath) {
-        // Warn on and remove invalid source files (if nonull was set).
-        if (!grunt.file.exists(filepath)) {
-          grunt.log.warn('Source file "' + filepath + '" not found.');
-          return false;
-        } else {
-          return true;
+
+      // Don't process an empty or nonexistant file
+      if(file.src.length === 0) { return; }
+
+      // Assign source file to Cheerio object,  so we can do jQuery-like manipulations
+      var $ = cheerio.load(grunt.file.read(file.src));
+
+      // Loop through every CSS link in header
+      // TODO: Change this so that only marked links are processed
+      $('link[rel="stylesheet"]').each(function () {
+        var csscontent;
+        var cssdomain;
+        var csspath;
+        var csscontents;
+
+        // Get href
+        var hrefinfo = $(this).attr('href');
+
+        // Parse domain
+        cssdomain = url.parse(hrefinfo).hostname;
+
+        // Parse path
+        csspath = url.parse(hrefinfo).pathname;
+
+        grunt.log.writeln(cssdomain);
+        grunt.log.writeln(csspath);
+
+
+        // Determine if initial csspath is relative or absolute
+        // TODO: how to handle relative paths? path.normalize?
+
+
+        // If basepath supplied, append to start of csspath
+        if (options.basepath) {
+          grunt.log.writeln('base path supplied');
         }
-      }).map(function (filepath) {
-        // Read file source.
-        return grunt.file.read(filepath);
-      }).join(grunt.util.normalizelf(options.separator));
 
-      // Handle options.
-      src += options.punctuation;
+        // If remotedomain supplied, append to start of path
+        if (options.basedomain) {
+          grunt.log.writeln('remote domain supplied');
+        }
 
-      // Write the destination file.
-      grunt.file.write(file.dest, src);
+        // If basedomain == cssdomain, then treat as filesystem path
 
-      // Print a success message.
-      grunt.log.writeln('File "' + file.dest + '" created.');
-    });
+
+        // Get contents of CSS file
+          // If contentpath is a URL, suck via something
+
+          // if contentpath is a filepath, grab via normal stuff
+
+
+        // Generate new inline contents
+          // <style></style>
+
+
+
+        $(this).replaceWith('<style>\n' + csscontents + '\n</style>');
+      }); // end foreach link
+
+      // what are the filenams being supplied?
+      // single file?
+      // single src+dest?
+      // array of single files
+      // array of src+dest
+
+
+      // Write all inline styles back to file
+      grunt.file.write(path.resolve(file.dest), $.html());
+
+
+
+    }); // end foreach file
+
+
+
+
+
   });
 
 };
